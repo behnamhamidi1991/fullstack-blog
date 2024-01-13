30ec595334f1c3a54490c14bdb99d42df4c5de91
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-14 01:51:49 +0330

    Callback

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
 <?php 
 
-$square = function ($number) {
-    return $number * $number;
-};
-
-$result = $square(5);
-
-echo "The square of 5 is " . $result;
-
-
-// Closures
-function createCounter() {
-    $count = 0;
-
-    $counter = function() use (&$count) {
-        return ++$count;
-    };
-
-    return $counter;
-}
-
-
-
 
 ?>
