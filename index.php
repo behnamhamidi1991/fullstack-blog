1e44f9fe941b0baabef10e27b71de0eec96baa72
Behnam Hamidi <eslp91@hotmail.com>
2024-03-14 16:43:56 +0330

    practice

--- index.php
+++ index.php
@@ -1,61 +1,13 @@
 <?php 
 
-abstract class Shape {
-    protected $name;
-
-    // Abstract method
-    abstract public function calculateArea();
+$num1 = 5;
+$num2 = "5";
+$fruit = "apple";
 
 
-    public function __construct($name) {
-        $this->name = $name;
-    }
+$result = $num1 + $num2;
 
-
-
-    // Concrete method
-    public function getName() {
-        return $this->name;
-    }
-
-}
-
-
-class Circle extends Shape {
-    private $radius;
-
-    public function __construct($name, $radius) {
-        parent::__construct($name);
-        $this->radius = $radius;
-    }
-
-    public function calculateArea() {
-        return pi() * pow($this->radius, 2);
-    }
-}
-
-class Rectangle extends Shape {
-    private $width;
-    private $height;
-
-    public function __construct($name, $height, $width) {
-        parent::__construct($name);
-        $this->height = $height;
-        $this->width = $width;
-    }
-
-    public function calculateArea() {
-        return $this->width * $this->height;
-    }
-}
-
-$circle = new Circle('Circle', 5);
-$rectangle = new Rectangle('Rectangle', 4, 6);
-
-echo $circle->getName() . ' Area: ' . $circle->calculateArea() . '<br>';
-echo $rectangle->getName() . ' Area: ' . $rectangle->calculateArea() . '<br>';
-
-
+var_dump($result)
 
 ?>
 
@@ -74,8 +26,16 @@ echo $rectangle->getName() . ' Area: ' . $rectangle->calculateArea() . '<br>';
 <body class="">
     <h1 class="course-title">PHP From Scratch</h1>
     <p>I am learning OOP right now and I will learn it like it is a piece of cake!</p>
-    <p>I will be the best computer engineer and developer the world has ever seen!</p>
-    <p>Just wait and watch!</p>
+    
+    <p> <?= "Hello, my name is " . $fullName ?> </p>
+
+    <br>
+    <br>
+    <p> <?= "Hello, my name is $fullName " ?> </p>
+
+    <br>
+    <br>
+    <p> <?= 'Hello, my name is $fullname' ?> </p>
 </body>
 
 </html>
