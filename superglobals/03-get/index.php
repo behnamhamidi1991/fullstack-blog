34e0142e91853d4ea419898a66407fe287113d9e
Behnam Hamidi <eslp91@hotmail.com>
2024-03-16 18:36:32 +0330

    global get

--- superglobals/03-get/index.php
+++ superglobals/03-get/index.php
@@ -1,3 +1,10 @@
 <?php
-// Add a query parameter to the URL in the browser's address bar:
-// http://localhost:8000/?name=John
+
+
+// echo isset($_GET['name']) ? $_GET['name'] : "Nothing";
+echo $_GET['name'] ?? '';
+
+// var_dump($_GET);
+
+
+?>
