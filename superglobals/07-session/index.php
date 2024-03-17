88b84c14128f908480bca11f69cf218554bced29
Behnam Hamidi <eslp91@hotmail.com>
2024-03-17 08:17:52 +0330

    session

---
+++ superglobals/07-session/index.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$_SESSION['name'] = "John";
+
+print_r($_SESSION);
+
+
+if (isset($_SESSION['name'])) {
+    echo "Name: " . $_SESSION['name'];
+} else {
+    echo "Name is not set";
+}
