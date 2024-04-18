6738744652dacacd169e0bafe27a1eb2c380e13a
Behnam Hamidi <eslp91@hotmail.com>
2024-04-18 15:51:49 +0330

    start

---
+++ README.md
@@ -0,0 +1,4 @@
+`First study the code you've written.`
+
+- then start from video 85
+  Start [60 minutes]
--- helpers.php
+++ helpers.php
@@ -25,7 +25,7 @@
    if (file_exists($viewPath)) {
       require $viewPath;
    } else {
-      echo "VIew '{$name}' not found!";
+      echo "View '{$name}' not found!";
    }
   }
 
@@ -44,6 +44,6 @@
    if (file_exists($partialPath)) {
       require $partialPath;
    } else {
-      echo "VIew '{$name}' not found!";
+      echo "View '{$name}' not found!";
    }
   }
