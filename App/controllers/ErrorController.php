85597309f0b27cf269822c7da0b7718fc86692ab
Behnam Hamidi <eslp91@hotmail.com>
2024-04-30 16:17:59 +0330

    Cleaning Up - Error page

--- App/controllers/ErrorController.php
+++ App/controllers/ErrorController.php
@@ -10,10 +10,24 @@ class ErrorController {
      * 
      * @return void
      */
-    public function notFound($message = 'Resource not found') {
-        http_response_code(404);
-        loadView('error', [
+    public static function notFound($message = 'Resource not found') {
+            http_response_code(404);
+            loadView('error', [
             'status' => '404',
+             'message' => $message
+        ]);
+    }
+
+    /**
+     * 403 not authorized error
+     * 
+     * @return void
+     */
+
+     public static function unauthorized($message = 'You are not authorized to see this resource!') {
+        http_response_code(403);
+        loadView('error', [
+            'status' => '403',
             'message' => $message
         ]);
     }
--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router {
     protected $routes = [];
 
@@ -67,17 +69,6 @@ class Router {
             $this->registerRoute('DELETE', $uri, $controller);
         }
 
-        /**
-         * Load error page 
-         * 
-         * @param int $httpCode
-         * @return void
-         */
-        public function error($httpCode = 404) {
-            http_response_code($httpCode);
-            loadView("error/{$httpCode}");
-            exit;
-        }
 
         /**
          * Route the request
@@ -99,6 +90,7 @@ class Router {
                     return;
                 }
             }
-            $this->error();
+            
+            ErrorController::notFound();
         }
 }
