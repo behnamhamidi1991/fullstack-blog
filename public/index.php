8efaeac89eaf3d1e91db00c8af3daa9018755910
Behnam Hamidi <eslp91@hotmail.com>
2024-04-30 11:19:33 +0330

    101 finished

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
-spl_autoload_register(function($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+use Framework\Router;
+
+// spl_autoload_register(function($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 
 // Instantiating the router 
