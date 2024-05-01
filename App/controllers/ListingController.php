53e312fcade68546fa01dd16ec691147a39d9f07
Behnam Hamidi <eslp91@hotmail.com>
2024-05-01 23:33:45 +0330

    sanitize

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -69,6 +69,10 @@ class ListingController
 
         $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
 
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
         inspectAndDie($newListingData);
     }
 }
