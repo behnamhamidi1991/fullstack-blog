29f268a2d65bd1bf538a3d5a17ec6f83c0306b4d
Behnam Hamidi <eslp91@hotmail.com>
2024-05-01 23:12:07 +0330

    controller

--- routes.php
+++ routes.php
@@ -3,4 +3,4 @@ $router->get('/', 'HomeController@index');
 $router->get('/listings', 'Listingcontroller@index');
 $router->get('/listings/create', 'Listingcontroller@create');
 $router->get('/listing/{id}', 'Listingcontroller@show');
-
+$router->post('/listings', 'Listingcontroller@store');
