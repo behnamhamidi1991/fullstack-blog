b4f6ca443c310c74734c7a16407b233f4597ac21
Behnam Hamidi <eslp91@hotmail.com>
2024-05-02 19:25:47 +0330

    session cookie

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -122,7 +122,21 @@ class UserController
             'city' => $city,
             'state' => $state
         ]);
+        redirect('/');
+    }
 
+    /**
+     * Logout a user and kill session
+     * 
+     * @return void
+     */
+
+    public function logout()
+    {
+        Session::clearAll();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
 
         redirect('/');
     }
