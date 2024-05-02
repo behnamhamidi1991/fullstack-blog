6aa6598c6b8d922bb784642f907ee9e1eaefb46b
Behnam Hamidi <eslp91@hotmail.com>
2024-05-02 19:09:57 +0330

    inspect and die

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\SEssion;
 
 class UserController
 {
@@ -110,6 +111,18 @@ class UserController
         ];
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // Get the new user ID
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state
+        ]);
+
 
         redirect('/');
     }
